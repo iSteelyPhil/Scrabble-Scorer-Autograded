f22134ed239f5104e536807dfb0b3e4c7caa4a3a
Phillip <philmography2010@gmail.com>
2024-07-09 15:33:08 -0500

    scrabble GA

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -21,37 +21,139 @@ function oldScrabbleScorer(word) {
 	  for (const pointValue in oldPointStructure) {
  
 		 if (oldPointStructure[pointValue].includes(word[i])) {
-			letterPoints += `Points for '${word[i]}': ${pointValue}\n`
+			letterPoints += `\nPoints for '${word[i]}': ${pointValue}\n`
 		 }
  
 	  }
 	}
 	return letterPoints;
  }
+/////
+let scrabbleScorer = function(word){
+   word = word.toLowerCase();
+   let letterPoints = "";
+   let score = 0;
+ 
+   for (let i = 0; i < word.length; i++) {
+     score += newPointStructure[word[i]];
+     letterPoints += `Points for '${word[i]}': ${newPointStructure[word[i]]}\n`;
+   }
+   console.log(letterPoints);
+   return score;
+ }
+ 
+/////
+function simpleScore(word){
+   let score = 0;
+   let stringAplhabet = "abcdefghijklmnopqrstuvwxyz"
+   word = word.toUpperCase();
+   for (let i = 0; i < word.length; i++) {
+      if (stringAplhabet.toUpperCase().includes(word[i])) {
+         score++;
+      }
+  }
+  return score;
+}
+/////
+function vowelBonusScore(word){
+   let score = 0;
+   let stringAplhabet = "abcdefghijklmnopqrstuvwxyz";
+   let stringVowels = "aeiouy";
+   word = word.toUpperCase();
+   for (let i = 0; i < word.length; i++) {
+      if (stringVowels.toUpperCase().includes(word[i])) {
+        score += 3;
+      } else if (stringAplhabet.toUpperCase().includes(word[i])) {
+         score++;
+  }
+}
+  return score;
+}
 
+/////
 // your job is to finish writing these functions and variables that we've named //
 // don't change the names or your program won't work as expected. //
 
 function initialPrompt() {
-   console.log("Let's play some scrabble! Enter a word:");
+   let word = input.question("Let's play some scrabble!\nEnter a word to score: ")
+   /* console.log(oldScrabbleScorer(word)); */
+   return word;
 };
 
-let newPointStructure;
 
-let simpleScorer;
+let simpleScorer = function(word){
+   let score = 0;
+   let stringAplhabet = "abcdefghijklmnopqrstuvwxyz"
+   word = word.toUpperCase();
+   for (let i = 0; i < word.length; i++) {
+      if (stringAplhabet.toUpperCase().includes(word[i])) {
+         score++;
+      }
+  }
+  return score;
+;}
 
-let vowelBonusScorer;
 
-let scrabbleScorer;
+let vowelBonusScorer = function(word){
+   let score = 0;
+   let stringAplhabet = "abcdefghijklmnopqrstuvwxyz";
+   let stringVowels = "aeiouy";
+   word = word.toUpperCase();
+   for (let i = 0; i < word.length; i++) {
+      if (stringVowels.toUpperCase().includes(word[i])) {
+      score += 3;
+      } else if (stringAplhabet.toUpperCase().includes(word[i])) {
+         score++;
+   }
+   }
+   return score;
+   }
 
-const scoringAlgorithms = [];
 
-function scorerPrompt() {}
 
-function transform() {};
+
+const scoringAlgorithms = [
+   {
+   Name: "Simple Scorer",
+   Description: "Each letter is worth 1 point.",
+   scorerFunction: simpleScore
+}, {
+   Name: "Bonus Vowels",
+   Description: "Vowels are 3 pts, consonants are 1 pt.",
+   scorerFunction: vowelBonusScore
+   
+}, {
+   Name: "Scrabble",
+   Description: "The traditional scoring algorithm",
+   scorerFunction: scrabbleScorer
+   } 
+];
+
+function scorerPrompt(word) {
+   console.log("Which scoring algorithm would you like to use?");
+   console.log("\n");
+   console.log("0 - Simple: One point per character\n1 - Vowel Bonus: Vowels are worth 3 points\n2 - Scrabble: Uses scrabble point system");
+   let choice = Number(input.question("Enter 0, 1, or 2: "));
+   scoreSystem = scoringAlgorithms[choice];
+   return console.log(`Score for ${word} : ${scoreSystem.scorerFunction(word)}`)
+};
+
+function transform(oldPointStructure) {
+   let newObj = {};
+  
+   for (let item in oldPointStructure) {
+     for (let i = 0; i < oldPointStructure[item].length; i++) {
+       newObj[oldPointStructure[item][i].toLowerCase()] = Number(item);
+     }
+   }
+   return newObj;
+ };
+
+let newPointStructure = transform(oldPointStructure);
 
 function runProgram() {
-   initialPrompt();
+   let word = initialPrompt();
+   scorerPrompt(word);
    
 }
 
